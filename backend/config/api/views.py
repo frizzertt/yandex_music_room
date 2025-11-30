253ccb3d9134420639f370cb7acdd1cefa2a3dee
Mikhail Zayats <mixaz2405@gmail.com>
2025-11-30 20:05:38 +0300

    fix: fix imports in views file

--- backend/config/api/views.py
+++ backend/config/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from serializers import RoomSerializer
-from models import Room
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 class RoomView(generics.CreateAPIView):
