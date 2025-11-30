77d1ae2e35ceacaf2cc087d698494de9c7a99903
Mikhail Zayats <mixaz2405@gmail.com>
2025-11-30 20:33:42 +0300

    update: update settings file, add rest_framework app

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
