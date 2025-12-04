d93260e416ef85bcaef5b01c8676ee6ec85d9340
Mikhail Zayats <mixaz2405@gmail.com>
2025-12-05 01:09:57 +0300

    feat: update urls for frontend

--- project/config/frontend/urls.py
+++ project/config/frontend/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import index
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    re_path(r'^join/?$', index),
+    re_path(r'^create/?$', index),
 ]
