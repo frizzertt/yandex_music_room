88d4090f5efaf2a2ffa4e608d405d67ec1a82e1e
Mikhail Zayats <mixaz2405@gmail.com>
2025-12-05 14:10:08 +0300

    feat: add url for create room view

--- project/config/api/urls.py
+++ project/config/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
     re_path(r'^room/?$', RoomView.as_view()),
+    re_path(r'^create-room/?$', CreateRoomView.as_view())
 ]
