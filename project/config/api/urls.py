f4aff1b07e75adb70a5350a966c8c2a04380a1b2
Mikhail Zayats <mixaz2405@gmail.com>
2025-12-06 21:13:22 +0300

    feat: add delete room url

--- project/config/api/urls.py
+++ project/config/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, DeleteRoom
 
 urlpatterns = [
     re_path(r'^room/?$', RoomView.as_view()),
     re_path(r'^create-room/?$', CreateRoomView.as_view()),
     re_path(r'^get-room/?$', GetRoom.as_view()),
     re_path(r'^join-room/?$', JoinRoom.as_view()),
+    re_path(r'^delete-room/?$', DeleteRoom.as_view()),
 ]
