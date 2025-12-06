92b77229bea864fe133278ae9636e3f368faf55b
Mikhail Zayats <mixaz2405@gmail.com>
2025-12-06 11:01:38 +0300

    feat: add url for get room api

--- project/config/api/urls.py
+++ project/config/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from .views import RoomView, CreateRoomView
+from .views import RoomView, CreateRoomView, GetRoom
 
 urlpatterns = [
     re_path(r'^room/?$', RoomView.as_view()),
-    re_path(r'^create-room/?$', CreateRoomView.as_view())
+    re_path(r'^create-room/?$', CreateRoomView.as_view()),
+    re_path(r'^get-room/?$', GetRoom.as_view()),
 ]
