d507215f281588bf9a4b93c0ce6471011aa63ae3
Mikhail Zayats <mixaz2405@gmail.com>
2025-12-06 19:58:22 +0300

    feat: add url for join room page

--- project/config/api/urls.py
+++ project/config/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 
 urlpatterns = [
     re_path(r'^room/?$', RoomView.as_view()),
     re_path(r'^create-room/?$', CreateRoomView.as_view()),
     re_path(r'^get-room/?$', GetRoom.as_view()),
+    re_path(r'^join-room/?$', JoinRoom.as_view()),
 ]
